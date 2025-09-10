fed84b9835c309a2ecc73b5190d1d58d8e55add2
fmvilas <fmvilas@gmail.com>
2025-09-10 11:44:25 +0200

    Change url for the styleguide

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/resources/style-guide" element={<StyleGuide />} />
+          <Route path="/resources/shift-communication-style-guide" element={<StyleGuide />} />
           <Route path="/resources/vista-framework" element={<VistaFramework />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
